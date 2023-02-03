d26542bd29866942ccd3c290425d93808e0a8779
Beny-Maleki <benyaminmaleki81@gmail.com>
2023-02-03 11:43:14 +0330

    Add runner scripts

---
+++ run_client.sh
@@ -0,0 +1,3 @@
+gcc -o client.out client.c -lpthread -w
+./client.out
+echo "Client is running..."
---
+++ run_server.sh
@@ -0,0 +1,3 @@
+gcc -o server.out server.c -lpthread -w
+echo "Server started up..."
+./server.out
